77c298d3960cfafc8984a514ca86771008a6572c
yheison0035 <yheison0035@gmail.com>
2026-09-14 11:17:23 -0500

    style(catalogo): catalogo mas ancho en desktop (max-w 1440px) para cards mas grandes

--- src/components/layout/catalogContainer.js
+++ src/components/layout/catalogContainer.js
@@ -1,6 +1,6 @@
 export default function CatalogContainer({ children }) {
   return (
-    <div className="max-w-295 mx-auto px-4 md:px-6 lg:px-8 pb-12">
+    <div className="max-w-[1440px] mx-auto px-4 md:px-6 lg:px-8 pb-12">
       {children}
     </div>
   );
